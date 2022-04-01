db52ec55c24223fd1a45a46a52923852a688848e
Shahid Ameen <43674745+iamshahidameen@users.noreply.github.com>
2022-04-01 21:53:32 +0500

    Added js functionality to insert correct keys into an array

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
-console.log("Hello, World!");
+const wordEl = document.getElementById('word');
+const wrongLetterEl = document.getElementById('wrong-letter');
+const playAgainBtn = document.getElementById('play-again');
+const popup = document.getElementById('popup-container');
+const notificationEl = document.getElementById('notification-container');
+const finalMessageEl = document.getElementById('final-message');
+
+const figureParts = document.querySelectorAll('figure-part');
+
+const words = ['application', 'programming', 'interface', 'wizard'];
+
+let selectedWord = words[Math.floor(Math.random() * words.length)];
+console.log(selectedWord);
+
+const correctLetters = [];
+const wrongLetters = [];
+displayWord();
+function displayWord() {
+    document.body.addEventListener('keyup', (e) => {
+        for(let i =0; i<= selectedWord.length; i++){
+            if(e.key == selectedWord[i]) {
+                
+                console.log('matched');
+                correctLetters[i] = e.key;
+                
+                
+
+
+            } else {
+                
+                
+            }
+        }
+        
+    })
+    console.log(correctLetters, 'he')
+    
+}
